5c95671d30b7ec7bd5a2a69bde8d126b90207441
nuwan-dev <nuwanc.email@gmail.com>
2024-02-27 17:30:14 +0530

    c

--- scripts/javascript/app.js
+++ scripts/javascript/app.js
@@ -75,5 +75,30 @@ $(document).ready(function () {
     autoplaySpeed: 800,
     //centerMode: true,
     centerPadding: "60px",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 });
